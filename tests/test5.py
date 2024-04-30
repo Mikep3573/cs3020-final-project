0abec591faf9760973b556cf4ec63055878ac942
dlaberg2 <112562809+dlaberg2@users.noreply.github.com>
2024-04-30 17:51:29 -0400

    Add files via upload
    Edited test 5 to clarify which add and mult methods are called

--- tests/test5.py
+++ tests/test5.py
@@ -2,22 +2,22 @@ class Point:
     x: int
     y: int
 
-    def add(self1: Point) -> int:
+    def addPoint(self1: Point) -> int:
         return self1.x + self1.y
 
-    def mult(self2: Point) -> int:
+    def multPoint(self2: Point) -> int:
         return self2.x * self2.y
 
 class Mark:
     x: int
     y: int
 
-    def add(self3: Mark) -> int:
+    def addMark(self3: Mark) -> int:
         return self3.x + self3.y
 
-    def mult(self4: Mark) -> int:
+    def multMark(self4: Mark) -> int:
         return self4.x * self4.y
 
 p = Point(1, 2)
 m = Mark(3, 4)
-print(p.add() + m.mult())
+print(p.addPoint() + m.addMark())
